2e3b0eb1d8b6afc3353e67d725518271419074d9
Piyush Kumar <Piyush.Kumar@wisetechglobal.com>
2023-05-12 10:41:04 +0530

    first commit for the blogging app-initial setup

---
+++ src/main/java/com/scaler/blogapi/comments/CommentsController.java
@@ -0,0 +1,10 @@
+package com.scaler.blogapi.comments;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blogs/{blogId}/comments")
+public class CommentsController {
+
+}
